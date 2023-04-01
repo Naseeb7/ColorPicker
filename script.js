469f06483405b96530f3dff55cec8a829e021b00
Pradosh <chandpradosh7@gmail.com>
2023-04-01 22:47:12 +0530

    Updated the css and added name changing function on button click

--- script.js
+++ script.js
@@ -1,44 +1,65 @@
 // Backgrounds
 Italianmarble=()=>{
     document.getElementById("background").style.backgroundColor="#e1dad2"
+    document.getElementById("background").innerHTML="Italian Marble"
 }
 tailwind=()=>{
     document.getElementById("background").style.backgroundColor="#e8e8e4"
+    document.getElementById("background").innerHTML="Tailwind"
 }
 HazelCream=()=>{
     document.getElementById("background").style.backgroundColor="#e4dddc"
+    document.getElementById("background").innerHTML="Hazel Cream"
 }
 Sailaway=()=>{
     document.getElementById("background").style.backgroundColor="#dde9ea"
+    document.getElementById("background").innerHTML="Sail Away"
 }
 DogwoodBlossom=()=>{
     document.getElementById("background").style.backgroundColor="#fdc8d1"
-}
-Wonderlake=()=>{
-    document.getElementById("background").style.backgroundColor="#60c6c6"
+    document.getElementById("background").innerHTML="Dogwood Blossom"
 }
 white=()=>{
     document.getElementById("background").style.backgroundColor="white"
+    document.getElementById("background").innerHTML="White"
 }
 Mint=()=>{
     document.getElementById("background").style.backgroundColor="#ADEFD1FF"
+    document.getElementById("background").innerHTML="Mint"
 }
 Silver=()=>{
     document.getElementById("background").style.backgroundColor="#A2A2A1FF"
+    document.getElementById("background").innerHTML="Silver"
 }
+
 // Pallete
 IrishGlory=()=>{
     document.getElementById("colorPallete").style.backgroundColor="#74cabf"
+    document.getElementById("colorPallete").innerHTML="Irish Glory"
 }
 Meanderinggarden=()=>{
     document.getElementById("colorPallete").style.backgroundColor="#90dec2"
+    document.getElementById("colorPallete").innerHTML="Meandering Garden"
 }
 Irisheyes=()=>{
     document.getElementById("colorPallete").style.backgroundColor="#ade8d0"
+    document.getElementById("colorPallete").innerHTML="Irish Eyes"
+}
+Wonderlake=()=>{
+    document.getElementById("colorPallete").style.backgroundColor="#60c6c6"
+    document.getElementById("colorPallete").innerHTML="Wonder Lake"
 }
 SkyBlue=()=>{
     document.getElementById("colorPallete").style.backgroundColor="#89ABE3FF"
+    document.getElementById("colorPallete").innerHTML="Sky Blue"
 }
 TurkishSea=()=>{
     document.getElementById("colorPallete").style.backgroundColor="#195190FF"
+    document.getElementById("colorPallete").innerHTML="Turkish Sea"
+}
+Clear=()=>{
+    document.getElementById("background").style.backgroundColor=""
+    document.getElementById("background").innerHTML="Background"
+    document.getElementById("colorPallete").style.backgroundColor=""
+    document.getElementById("colorPallete").innerHTML="Pallete"
 }
